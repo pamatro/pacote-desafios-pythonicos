752d42d61b4cb46027d88cfe7e388df5eb49dcf7
pamatro <60119603+pamatro@users.noreply.github.com>
2021-02-07 17:55:14 -0300

    Update 04_mix_up.py

--- 04_mix_up.py
+++ 04_mix_up.py
@@ -13,8 +13,10 @@ Assuma que a e b tem tamanho 2 ou maior.
 """
 
 def mix_up(a, b):
-    # +++ SUA SOLUÇÃO +++
-    return
+    c = a
+    a = b[:2] + a[2:]
+    b = c[:2] + b[2:]
+    return ' '.join([a, b])
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
